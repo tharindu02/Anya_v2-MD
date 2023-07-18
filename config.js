2b7374d7a2f316087c0845d9353c3deaba6dc927
tharindu02 <139851896+tharindu02@users.noreply.github.com>
2023-07-18 13:08:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -34,11 +34,11 @@ global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
-global.ytname = "YT: Enter Your Channel Name Here"
+global.ownername = "Dew del zap"    
+global.ownernumber = ["94704078221"]
+global.ytname = "YT: RS teachshow"
 global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
+global.region = "Srilanka"
 global.state = "Enter Your State Name Here" // your state name
 global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
 global.instagramId = "Enter Your Insta ID Here"
